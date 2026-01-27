7d7b381c598bf72897cdff84a901869698dab0a8
transontri2006 <transontri2006.fpt@gmail.com>
2026-01-27 19:52:29 +0700

    Update enha.java

--- src/uanhunao/enha.java
+++ src/uanhunao/enha.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package uanhunao;
 
 import java.util.Scanner;
@@ -22,4 +19,5 @@ public class enha {
         System.out.println("So lan xuat hien cua"+ch+":"+count);
     }
 }
+
 
