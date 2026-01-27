1dbe4521b9651cdd895cceaf10de08caf8bb7208
transontri2006 <transontri2006.fpt@gmail.com>
2026-01-27 19:53:05 +0700

    Update homquasao.java

--- src/uanhunao/homquasao.java
+++ src/uanhunao/homquasao.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package uanhunao;
 
 import java.util.Scanner;
@@ -16,3 +13,4 @@ public class homquasao {
         System.out.println(result);
     }
 }
+
